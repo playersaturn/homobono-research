cbde18ec4e7cbab6924677b89c1af9f8cf1c0821
Eduardo <eduardobuennogm@gmail.com>
2025-12-10 01:57:19 -0300

    Fix Stripe API version and admin build errors

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -2,7 +2,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? 'mock_key', {
-    apiVersion: '2025-01-27.acacia',
+    apiVersion: '2025-11-17.clover', // Updated to match installed types
     appInfo: {
         name: 'InvestTips Newsletter',
         version: '0.1.0'
